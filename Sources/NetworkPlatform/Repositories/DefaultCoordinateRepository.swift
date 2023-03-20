bc0a8b75d31bfca6892927b00e9c030f4da76de5
Donguk Kim <ehddnr73@naver.com>
2023-03-20 22:48:00 +0900

    CoordinateRepositoryError 정의
    - fetchCoordinate(query: CoordinateQuery) async throws -> Coordinate 메서드에 사용.

--- Sources/NetworkPlatform/Repositories/DefaultCoordinateRepository.swift
+++ Sources/NetworkPlatform/Repositories/DefaultCoordinateRepository.swift
@@ -7,15 +7,24 @@
 
 import Foundation
 import Domain
+import GoogleMapsGeoCodingSwift
+
+public enum CoordinateRepositoryError: String, Error {
+    case dataTransferServiceError = "네트워크 통신 구성에서 에러가 발생했습니다."
+}
 
 public struct DefaultCoordinateRepository: CoordinateRepository {
-    private let dataTransferService: DataTransferService
+    private let dataTransferService: DataTransferService?
     
     public init() {
         self.dataTransferService = DefaultDataTransferService()
     }
     
     public func fetchCoordinate(query: CoordinateQuery) async throws -> Coordinate {
-        try await dataTransferService.request()
+        if let dataTransferService = dataTransferService {
+            return try await dataTransferService.request()
+        } else {
+            throw CoordinateRepositoryError.dataTransferServiceError
+        }
     }
 }
