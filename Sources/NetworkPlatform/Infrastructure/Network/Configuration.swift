fdd68c4981dc9c3d748ab2ad3068b9d3464cacaa
Donguk Kim <ehddnr73@naver.com>
2023-03-21 04:37:12 +0900

    Configure struct 정의

---
+++ Sources/NetworkPlatform/Infrastructure/Network/Configuration.swift
@@ -0,0 +1,16 @@
+//
+//  Configuration.swift
+//  
+//
+//  Created by 김동욱 on 2023/03/21.
+//
+
+import Foundation
+
+public struct Configuration {
+    let key: String
+    
+    public init(key: String) {
+        self.key = key
+    }
+}
